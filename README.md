1d78351ccf665e41ff15fb01d26b32bb197f059a
Maciej Retowski <retowskim@gmail.com>
2018-05-17 14:29:58 +0200

    added short readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-Posprzątałem trochę, w articles/ są wszystkie niesformatowane artykuły w .txt, w data wszystkie dane wygenerowane z pythonow, gotowe do przemielenia przez matlaba, w som_maps wszystkie ostateczne mapy z matlaba. Złożyłem wszystkie skrypty w pythonie w jeden - script.py
+Aby przygotować interpreter dla pythona należy sciągnąć anaconde, stworzyć środowisko dla pliku environment.yml, i aktywować 'source activate si'. Plik script.py generuje z artykułów w folderze article/ dane w formacie odpowiednim dla skryptu w octave som.m. Python generuje następujące dane 
+ - ./data/basic_data/
+   - zsumowane wyniki dla osobno artykułów sugerujące katastrofę i zamach
+   - zsumowane dane razem (katastrofa + zamach)
+   - dane na podstawie artykułów z Wikipedii które teoretycznie powinny być neutralne
+ - ./data/biased_assasination/
+ - ./data/biased_catastrophy/
+   W obu folderach znajdują się foldery part i multiply.
+   W folderach part znajdują się dane zsumowane z danych netruralnych + (i/30) * biased.
+   Teoretycznie dane te powinny pokazywać jak poglądy neutralne 
+   powoli zmieniają się biased.
+   W folderach multiply znajdują się zsumowane dane neutral + i * biased.
+ - ./data/single_articles_data/
+   Dane dla pojedynczych artykułów z articles/. Na ich podstawie można sprawdzać czy 
+   rzeczywiście pojedyncze artykuły są bliżej danych zsumowanych w ./data/basic_data
+Skrypt w octavie generuje odpowiednie mapy w formacie .jpg do folderu som_maps/ oraz porównuje numerycznie 
+wektory w somach, na ich podstawie można sprawdzać czy somy są podobne czy nie. Wyniki porównań są w ./numerical_compare/
+
--- som.m
+++ som.m
@@ -46,54 +46,54 @@ print('./som_maps/basic_maps/catastrophy.jpg')
 
 
 
-## files_x = cell
-## files_x{1} = ('./data/basic_data/correct_catastrophy.dat');
-## files_x{2} = ('./data/basic_data/neutral_from_wiki.dat');
-## files_x{3} = ('./data/basic_data/correct_assasination.dat');
-## files_x{4} = ('./data/basic_data/neutral_from_wiki.dat');
-## compareFiles(files_x,files_x, 'numerical_compare/basic_comparison.csv');
-## files_catastrophy = glob('./data/single_articles_data/catastrophy/*');
-## compareFiles(files_catastrophy,files_x, 'numerical_compare/single_articles_catastrophy.csv');
-## files_assasination = glob('./data/single_articles_data/assasination/*');
-## compareFiles(files_assasination,files_x, 'numerical_compare/single_articles_assasination.csv');
+files_x = cell
+files_x{1} = ('./data/basic_data/correct_catastrophy.dat');
+files_x{2} = ('./data/basic_data/neutral_from_wiki.dat');
+files_x{3} = ('./data/basic_data/correct_assasination.dat');
+files_x{4} = ('./data/basic_data/neutral_from_wiki.dat');
+compareFiles(files_x,files_x, 'numerical_compare/basic_comparison.csv');
+files_catastrophy = glob('./data/single_articles_data/catastrophy/*');
+compareFiles(files_catastrophy,files_x, 'numerical_compare/single_articles_catastrophy.csv');
+files_assasination = glob('./data/single_articles_data/assasination/*');
+compareFiles(files_assasination,files_x, 'numerical_compare/single_articles_assasination.csv');
 
 
 
-## files = glob('./data/biased_catastrophy/part/*')
-## diffrences = cell(numel(files), 9)
-## for i=1:numel(files)
-##   [~, name] = fileparts (files{i});
-##   full_name = ['./som_maps/biased_catastrophy_part/', name, '.jpg']
-##   dir_data = files{i};
-##   sD = som_read_data(dir_data);
-##   sM = som_make(sD, 'msize', [12, 12], 'lattice', 'hexa');
-##   sM = som_autolabel(sM,sD);
-##   [current_unit_diffrence, current_l2_diffrence] = get_diffrences(sM, keywords);
-##   diffrences(i, 1) = comp_diffrence(current_unit_diffrence, neutral_unit_diffrence);
-##   diffrences(i, 2) = comp_diffrence(current_unit_diffrence, catastrophy_unit_diffrence);
-##   diffrences(i, 3) = comp_diffrence(current_unit_diffrence, assasination_unit_diffrence);
-##   diffrences(i, 4) = comp_diffrence(current_unit_diffrence, last_unit_diffrence);
-##   diffrences(i, 5) = comp_diffrence(current_l2_diffrence, neutral_l2_diffrence);
-##   diffrences(i, 6) = comp_diffrence(current_l2_diffrence, catastrophy_l2_diffrence);
-##   diffrences(i, 7) = comp_diffrence(current_l2_diffrence, assasination_l2_diffrence);
-##   diffrences(i, 8) = comp_diffrence(current_l2_diffrence, last_l2_diffrence);
-##   diffrences(i, 9) = name;
-##   last_unit_diffrence = current_unit_diffrence;
-##   last_l2_diffrence = current_l2_diffrence;
-##   som_show(sM,'umat','all','empty','wszystkie');
-##   som_show_add('label',sM,'TextSize',5,'subplot','all');
-##   som_show(sM,'comp',18);
-##   som_show_add('label',sM,'TextSize',5,'TextColor', 'w', 'subplot',1);
-##   print(full_name)
-## endfor
-## fid = fopen('./numerical_compare/catastrophy_part.csv', 'w')
-## fprintf(fid, 'neutral_unit, catastrophy_unit, assasination_unit last_unit, neutral_l2, catastrophy_l2, assasination_l2, last_l2 \n');
-## for i=1:numel(files)
-##   fprintf(fid, '%s, ', diffrences{i,9}) ;
-##   fprintf(fid, '%f, ', diffrences{i,1:7}) ;
-##   fprintf(fid, '\n') ;
-## endfor
-## fclose(fid) ;
+files = glob('./data/biased_catastrophy/part/*')
+diffrences = cell(numel(files), 9)
+for i=1:numel(files)
+  [~, name] = fileparts (files{i});
+  full_name = ['./som_maps/biased_catastrophy_part/', name, '.jpg']
+  dir_data = files{i};
+  sD = som_read_data(dir_data);
+  sM = som_make(sD, 'msize', [12, 12], 'lattice', 'hexa');
+  sM = som_autolabel(sM,sD);
+  [current_unit_diffrence, current_l2_diffrence] = get_diffrences(sM, keywords);
+  diffrences(i, 1) = comp_diffrence(current_unit_diffrence, neutral_unit_diffrence);
+  diffrences(i, 2) = comp_diffrence(current_unit_diffrence, catastrophy_unit_diffrence);
+  diffrences(i, 3) = comp_diffrence(current_unit_diffrence, assasination_unit_diffrence);
+  diffrences(i, 4) = comp_diffrence(current_unit_diffrence, last_unit_diffrence);
+  diffrences(i, 5) = comp_diffrence(current_l2_diffrence, neutral_l2_diffrence);
+  diffrences(i, 6) = comp_diffrence(current_l2_diffrence, catastrophy_l2_diffrence);
+  diffrences(i, 7) = comp_diffrence(current_l2_diffrence, assasination_l2_diffrence);
+  diffrences(i, 8) = comp_diffrence(current_l2_diffrence, last_l2_diffrence);
+  diffrences(i, 9) = name;
+  last_unit_diffrence = current_unit_diffrence;
+  last_l2_diffrence = current_l2_diffrence;
+  som_show(sM,'umat','all','empty','wszystkie');
+  som_show_add('label',sM,'TextSize',5,'subplot','all');
+  som_show(sM,'comp',18);
+  som_show_add('label',sM,'TextSize',5,'TextColor', 'w', 'subplot',1);
+  print(full_name)
+endfor
+fid = fopen('./numerical_compare/catastrophy_part.csv', 'w')
+fprintf(fid, 'neutral_unit, catastrophy_unit, assasination_unit last_unit, neutral_l2, catastrophy_l2, assasination_l2, last_l2 \n');
+for i=1:numel(files)
+  fprintf(fid, '%s, ', diffrences{i,9}) ;
+  fprintf(fid, '%f, ', diffrences{i,1:7}) ;
+  fprintf(fid, '\n') ;
+endfor
+fclose(fid) ;
 
 
 
@@ -126,7 +126,6 @@ for i=1:numel(files)
   print(full_name)
 endfor
 fid = fopen('./numerical_compare/catastrophy_multiply.csv', 'w')
-## fid = fopen('./numerical_compare/catastrophy_multiply.csv', 'w')
 fprintf(fid, 'neutral_unit, catastrophy_unit, assasination_unit last_unit, neutral_l2, catastrophy_l2, assasination_l2, last_l2 \n');
 for i=1:numel(files)
   fprintf(fid, '%s, ', diffrences{i,9}) ;
