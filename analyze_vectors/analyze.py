c6012fc12cb37a74f496f4463dc5bf2730e80fec
alako <alako@github.com>
2018-05-28 11:48:07 +0200

    nowa analiza wektorow

--- analyze_vectors/analyze.py
+++ analyze_vectors/analyze.py
@@ -2,15 +2,20 @@ import pandas as pd
 
 def keywords_with_the_most_important_words(n=5):
     if n == 50:
-        file = open("./analyze_after_cut/catastrophy_keywords_50.txt", "w")
-        #file = open("./analyze_after_cut/assasination_keywords_50.txt", "w")
-        #file = open("./analyze_after_cut/neutral_keywords_50.txt", "w")
-    else:
-        file = open("./analyze_after_cut/catastrophy_keywords_5.txt", "w")
-        #file = open("./analyze_after_cut/assasination_keywords_5.txt", "w")
-        #file = open("./analyze_after_cut/neutral_keywords_5.txt", "w")
-    # data = pd.read_csv('./analyze_after_cut/vector_catastrophy')
-    data = pd.read_csv('./analyze_after_cut/vector_assasination')
+        #file = open("catastrophy_keywords_50.txt", "w")
+        #file = open("assasination_keywords_50.txt", "w")
+        file = open("neutral_keywords_50.txt", "w")
+    if n == 10:
+        #file = open("catastrophy_keywords_10.txt", "w")
+        #file = open("assasination_keywords_10.txt", "w")
+        file = open("neutral_keywords_10.txt", "w")
+    if n == 5:
+        #file = open("catastrophy_keywords_5.txt", "w")
+        #file = open("assasination_keywords_5.txt", "w")
+        file = open("neutral_keywords_5.txt", "w")
+    #data = pd.read_csv('vector_catastrophy')
+    #data = pd.read_csv('vector_assasination')
+    data = pd.read_csv('vector_neutral')
     for index, row in data.iterrows():
         file.write("\n\nKLUCZOWE SŁOWO: {}\n".format(row[0]))
         row[0] = 0
@@ -19,19 +24,19 @@ def keywords_with_the_most_important_words(n=5):
 
 
 def analyze_similarity_of_keywords_vectors():
-    file = open("./analyze_after_cut/catastrophy_similarity.txt", "w")
-    #file = open("./analyze_after_cut/assasination_similarity.txt", "w")
-    #file = open("./analyze_after_cut/neutral_similarity.txt", "w")
+    file = open("catastrophy_similarity.txt", "w")
+    #file = open("assasination_similarity.txt", "w")
+    #file = open("neutral_similarity.txt", "w")
 
-    data = pd.read_csv('./analyze_after_cut/vector_catastrophy')
-    #data = pd.read_csv('./analyze_after_cut/vector_assasination')
-    #data = pd.read_csv('./analyze_after_cut/vector_neutral')
+    data = pd.read_csv('vector_catastrophy')
+    #data = pd.read_csv('vector_assasination')
+    #data = pd.read_csv('vector_neutral')
 
     list = []
     cnt, cnt2 = 0, 0
     for column in data:
         file.write("\n"+column+"\n")
-        if data[column].max() != 'zdrad':
+        if data[column].max() != 'zdrada':
             x = float(data[column].max())/float(data[column].sum())
             x *= 100
             list.append(x)
@@ -46,5 +51,6 @@ def analyze_similarity_of_keywords_vectors():
 
 if __name__ == "__main__":
     keywords_with_the_most_important_words(5)
+    keywords_with_the_most_important_words(10)
     keywords_with_the_most_important_words(50)
-    analyze_similarity_of_keywords_vectors()
+    #analyze_similarity_of_keywords_vectors()
